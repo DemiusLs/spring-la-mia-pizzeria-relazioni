b58026192d875fff32bf93f2b67c879a809e58b8
DemiusLs <dloss320@gmail.com>
2025-10-22 10:42:18 +0200

    feat: project init

---
+++ src/main/java/org/lesson/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,106 @@
+package org.lesson.java.spring.spring_la_mia_pizzeria_crud.controller;
+
+import java.util.List;
+
+import org.lesson.java.spring.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.lesson.java.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@RequestMapping("/pizzas")
+public class PizzaController {
+
+    @Autowired
+    PizzaRepository repo;
+    
+    @GetMapping("/index")
+    public String index(Model model) {
+        List<Pizza> pizzas = repo.findAll();
+        model.addAttribute("pizzas" , pizzas );
+        return "/pizzas/index";
+    }
+
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id , Model model ){
+
+        Pizza pizza = repo.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "/pizzas/show";
+    }
+
+
+    @GetMapping("/searchByName")
+    public String searchByName(@RequestParam(name ="name", required = false) String name , Model model ){
+
+        List<Pizza> pizzas ;
+        if(name != null && !name.isEmpty()){
+            pizzas = repo.findByNameIgnoreCaseContaining(name); 
+        }else{
+            pizzas = repo.findAll();
+        }
+        model.addAttribute("pizzas", pizzas);
+        return "/pizzas/index";
+    }
+
+
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("pizza", new Pizza());
+        return "/pizzas/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza,BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "pizzas/create";
+        }  
+            repo.save(formPizza);
+
+        return "redirect:/pizzas/index";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id,  Model model ){
+        model.addAttribute("pizza", repo.findById(id).get());
+        return "/pizzas/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("pizza") Pizza formPizza,BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "pizzas/edit";
+        }  
+            repo.save(formPizza);
+
+        return "redirect:/pizzas/index";
+    }
+
+
+     @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        
+        repo.deleteById(id);
+        
+        return "redirect:/pizzas/index";
+    }
+
+
+
+}
---
+++ src/main/java/org/lesson/java/spring/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,87 @@
+package org.lesson.java.spring.spring_la_mia_pizzeria_crud.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+
+    //attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;    
+
+    @NotBlank(message = "The name can't be null, empty or blank")
+    @Size(min=1 ,max=50 , message = "The name size must be longer than 1 and shorter than 50")
+    private String name;
+    @NotBlank(message = "The description can't be null, empty or blank")
+     @Size(min=10 ,max=150 , message = "The description size must be longer than 10 and shorter than 150")
+    private String description;
+    @NotBlank(message = "The url can't be null, empty or blank")
+     @Size(min=1 ,max=50 , message = "The url size must be longer than 1 and shorter than 50")
+    private String url;
+    @DecimalMin( value ="0.0", inclusive= false , message = "Price must be positive"  )
+    private BigDecimal price;
+
+
+    //getter aand setter
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    
+
+    @Override
+    public String toString(){
+
+        return String.format("%s Descrizione: %s, Prezzo: %s",  name , description , price);
+    }
+
+}
